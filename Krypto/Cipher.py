2274621d0803277fb623962e2fcba67c7dc10b00
Sumerin <sumekpl@gmail.com>
2019-11-14 21:19:24 +0100

    column cipher

--- Krypto/Cipher.py
+++ Krypto/Cipher.py
@@ -1,5 +1,4 @@
-import queue
-
+import queue, locale
 
 def railFence_encipher(message, h):
     layers = {}
@@ -93,6 +92,85 @@ def matrix_cipher(message, w):
         for col in range(w):
             for row in range(w):
                 result.append(matrix[row][col])
+
+    return "".join(result)
+
+
+def column_key_change(key):
+    n = len(key)
+    locale.setlocale(locale.LC_COLLATE, "pl_PL.UTF-8")
+    sorted_key = sorted(key, key=locale.strxfrm)
+    result = {}
+    key_dict = {i: key[i] for i in range(n)}
+    for i in range(n):
+        char = sorted_key[i]
+        for (k, v) in key_dict.items():
+            if v == char:
+                result[i] = k
+                del key_dict[k]
+                break
+
+    return result, n
+
+
+def column_encipher(message, key):
+
+    pi, n = column_key_change(key)
+    return column_encipher_pi_n(message, pi, n)
+
+
+def column_encipher_pi_n(message, pi, n):
+
+    matrix = []
+    for _ in range(n):
+        matrix.append([])
+
+    for i in range(len(message)):
+        char = message[i]
+        idx = i % n
+        matrix[idx].append(char)
+
+    result = []
+    for i in range(n):
+        idx = pi[i]
+        column_value = "".join(matrix[idx])
+        result.append(column_value)
+
+    return "".join(result)
+
+
+def column_decipher(message, key):
+
+    pi, n = column_key_change(key)
+    return column_decipher_pi_n(message, pi, n)
+
+
+def column_decipher_pi_n(message, pi, n):
+
+    col_count = int(len(message)/n)
+    col_with_extra_element = len(message) % n
+
+    matrix = []
+    for _ in range(n):
+        matrix.append([])
+
+    pos = 0
+    for i in range(n):
+        start = pos
+        end = pos + col_count
+        matrix_idx = pi[i]
+        if matrix_idx < col_with_extra_element:
+            end += 1
+        matrix[matrix_idx] = message[start:end]
+        pos = end
+
+    result = []
+    for row in range(col_count):
+        for col in range(n):
+            result.append(matrix[col][row])
+
+    for col in range(col_with_extra_element):
+        result.append(matrix[col][col_count])
 
     return "".join(result)
 
--- UnitTests/CipherUnitTests.py
+++ UnitTests/CipherUnitTests.py
@@ -43,11 +43,67 @@ class CipherMethods(unittest.TestCase):
         self.assertEquals(expected, result)
 
     def test_matrix_decipher_w3(self):
-        expected = "SZYFR KWADRATOWY NAZYWANY JEST TAKŻE MACIERZOWYM"
+        expected = "SZYFR KWADRATOWY NAZYWANY JEST TAKŻE MACIERZOWYMAĄBCĆD"
         encrypted = "SFKZRWY ADTYRO AWNAWYZA YNJE KSTŻTAE CRMIZAEOWACYĄĆMBD"
 
         result = Cipher.matrix_cipher(encrypted, w=3)
         self.assertEquals(expected, result)
+
+    def test_column_encipher(self):
+        expected = "RROAFGAPYEB ZWZMSOCY NIZMMLSEU WRLTRTOSEEKEIM JPAU "
+        message = "PIERWSZYM PARAMETREM SZYFRU KOLUMNOWEGO JEST LICZBA"
+        key = "JIHGFĘEDĆCBĄA"
+
+        result = Cipher.column_encipher(message, key)
+        self.assertEquals(expected, result)
+
+    def test_column_encipher_pi_n(self):
+        expected = "RROAFGAPYEB ZWZMSOCY NIZMMLSEU WRLTRTOSEEKEIM JPAU "
+        message = "PIERWSZYM PARAMETREM SZYFRU KOLUMNOWEGO JEST LICZBA"
+        pi = {
+            0: 12,
+            1: 11,
+            2: 10,
+            3: 9,
+            4: 8,
+            5: 7,
+            6: 6,
+            7: 5,
+            8: 4,
+            9: 3,
+            10: 2,
+            11: 1,
+            12: 0,
+        }
+        n = 13
+
+        result = Cipher.column_encipher_pi_n(message, pi, n)
+        self.assertEquals(expected, result)
+
+    def test_column_encipher2(self):
+        expected = "SIEIRDDNRMHMUGAAMTMES"
+        message = "AMIDSUMMERNIGHTSDREAM"
+        key = "SWINDON"
+
+        result = Cipher.column_encipher(message, key)
+        self.assertEquals(expected, result)
+
+    def test_column_decipher(self):
+        expected = "PIERWSZYM PARAMETREM SZYFRU KOLUMNOWEGO JEST LICZBA"
+        encrypted = "RROAFGAPYEB ZWZMSOCY NIZMMLSEU WRLTRTOSEEKEIM JPAU "
+        key = "JIHGFĘEDĆCBĄA"
+
+        result = Cipher.column_decipher(encrypted, key)
+        self.assertEquals(expected, result)
+
+    def test_column_decipher2(self):
+        expected = "AMIDSUMMERNIGHTSDREAM"
+        encrypted = "SIEIRDDNRMHMUGAAMTMES"
+        key = "SWINDON"
+
+        result = Cipher.column_decipher(encrypted, key)
+        self.assertEquals(expected, result)
+
 
 if __name__ == '__main__':
     unittest.main()
