8679166238126f88a11078bb660ab117a51b0ab3
Andrzej Bała <andrzejbala@gmail.com>
2019-11-15 21:55:57 +0100

    Modular Arithmetic divisors & divisors count

--- Krypto/Modular_Arithmetic.py
+++ Krypto/Modular_Arithmetic.py
@@ -13,3 +13,25 @@ def pow_modulo(a, n, mod):
         a = mul_modulo(a, a, mod)
         n = math.floor(n/2)
     return c
+
+
+def divisors(a):
+    d = {1, a}
+    for b in range(2, math.floor(math.sqrt(a))+1):
+        "range do not include end value, +1 is necessary"
+        if a % b == 0:
+            d.add(b)
+            d.add(int(a/b))
+    return d
+
+
+def divisors_count(a):
+    if a == 1:
+        return 1
+    d = 2
+    for b in range(2, math.floor(math.sqrt(a)) + 1):
+        "range do not include end value, +1 is necessary"
+        if a % b == 0:
+            d += 2
+    return d
+
